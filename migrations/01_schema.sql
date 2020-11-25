c660be0b37d6378e17d39ace9101a8fabef0b696
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-25 13:20:29 -0800

    Update to LH version: I missed several constraints, defaults etc.
    - add NOT NULLs for critical info
    - add DEFAULTs for simple values
    - add ON DELETE CASCADE for FKs

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -6,45 +6,49 @@ DROP TABLE if EXISTS users CASCADE;
 
 -- CREATE TABLEs in order of least dependent to most dependent
 CREATE TABLE "users" (
-  "id" SERIAL PRIMARY KEY,
-  "name" VARCHAR(255),
-  "email" VARCHAR(255),
-  "password" VARCHAR(255)
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "name" VARCHAR(255) NOT NULL,
+  "email" VARCHAR(255) NOT NULL,
+  "password" VARCHAR(255)  NOT NULL
 );
 
 CREATE TABLE "properties" (
-  "id" SERIAL PRIMARY KEY,
-  "owner_id" INTEGER REFERENCES users(id),
-  "title" TEXT,
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "owner_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
+
+  "title" TEXT NOT NULL,
   "description" TEXT,
-  "thumbnail_url" VARCHAR(255),
-  "coverphoto_url" VARCHAR(255),
-  "cost_per_night" INTEGER,
-  "parking_spaces" INTEGER,
-  "number_of_bathrooms" INTEGER,
-  "number_of_bedrooms" INTEGER,
-  "country" VARCHAR(255),
-  "street" VARCHAR(255),
-  "city" VARCHAR(255),
-  "province" VARCHAR(255),
-  "post_code" VARCHAR(255),
-  "active" BOOLEAN
+
+  "thumbnail_url" VARCHAR(255) NOT NULL,
+  "coverphoto_url" VARCHAR(255) NOT NULL,
+  "cost_per_night" INTEGER NOT NULL DEFAULT 0,
+  "parking_spaces" INTEGER NOT NULL DEFAULT 0,
+  "number_of_bathrooms" INTEGER NOT NULL DEFAULT 0,
+  "number_of_bedrooms" INTEGER NOT NULL DEFAULT 0,
+
+  "country" VARCHAR(255) NOT NULL,
+  "street" VARCHAR(255) NOT NULL,
+  "city" VARCHAR(255) NOT NULL,
+  "province" VARCHAR(255) NOT NULL,
+  "post_code" VARCHAR(255) NOT NULL,
+
+  "active" BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE "reservations" (
   "id" SERIAL PRIMARY KEY,
-  "start_date" DATE,
-  "end_date" DATE,
-  "guest_id" INTEGER REFERENCES users(id),
-  "property_id" INTEGER REFERENCES properties(id)
+  "start_date" DATE NOT NULL,
+  "end_date" DATE NOT NULL,
+  "guest_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  "property_id" INTEGER REFERENCES properties(id) ON DELETE CASCADE
 );
 
 CREATE TABLE "property_reviews" (
   "id" SERIAL PRIMARY KEY,
-  "guest_id" INTEGER REFERENCES users(id),
-  "property_id" INTEGER REFERENCES properties(id),
-  "reservation_id" INTEGER REFERENCES reservations(id),
-  "rating" SMALLINT,
+  "guest_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  "property_id" INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  "reservation_id" INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  "rating" SMALLINT NOT NULL DEFAULT 0,
   "message" TEXT
 );
 
